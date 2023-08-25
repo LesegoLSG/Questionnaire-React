c6ce2ced55743e293b52659d0c4b32b63ed07011
LesegoLSG <217057620@student.uj.ac.za>
2023-08-24 23:42:13 -0500

    Components to be displayed when there are no questions in the list

---
+++ src/Components/NoQuestion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoQuestion = () =>{
+    return(
+        <div>
+            <h1>No Questions,click on add button to add question</h1>
+        </div>
+    );
+};
+
+export default NoQuestion;
