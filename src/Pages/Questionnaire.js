c32422f1ec74517a45f8ebc5f18213cd29dcbc84
LesegoLSG <217057620@student.uj.ac.za>
2023-08-27 15:35:59 -0500

    Added methods for fetching data, set data/questions to the current state and calling QuizList

--- src/Pages/Questionnaire.js
+++ src/Pages/Questionnaire.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Quiz from '../Components/Quiz';
+import { useState, useEffect} from 'react';
+import QuizList from '../Components/QuizList';
+
 
 const Questionnaire = () =>{
+    
+    const [questions, setQuestions] = useState([]);
+    //Loading data from server when page loads/refreshes
+    useEffect(() =>{
+        const getQuestions = async () =>{
+            const questionsFromServer = await fetchQuestions();
+            
+            setQuestions(questionsFromServer);
+            //console.log(questionsFromServer);
+        }
+        getQuestions();
+    },[]);
+
+    //Fetching questions from json-server
+    const fetchQuestions = async () =>{
+        const res = await fetch('http://localhost:5001/questions');
+        const data = await res.json();
+        
+        console.log(data);
+        return data;
+    }
+    
     return(
-        <div>
-            <h1>Questionnaire !</h1>
-        </div>
+        <QuizList questions={questions} />
     );
 };
 
