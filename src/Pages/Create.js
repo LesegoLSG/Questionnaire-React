e28386db64ad840687448e1f21117144615c9115
LesegoLSG <217057620@student.uj.ac.za>
2023-08-27 06:23:12 -0500

    Modified the add and delete functionality

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -3,36 +3,52 @@ import Add from '../Components/Add';
 import Question from '../Components/Question';
 import QuestionList from '../Components/QuestionList';
 import NoQuestion from '../Components/NoQuestion';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Create = () =>{
-    const [questions , setQuestions]= useState( [
-        {
-            id:1,
-            text:'How old are you???'
-        },
+    const [questions , setQuestions]= useState( []);
 
-        {
-            id:2,
-            text:'Where do you stay?'
-        },
+    useEffect(() =>{
+        const getQuestions = async () =>{
+            const questionsFromServer = await fetchQuestions();
+            setQuestions(questionsFromServer);
+        }
+        
+        getQuestions();
+    },[]);
 
-        {
-            id:3,
-            text:'last questionttttttttttttttttt?'
-        },
-    ]);
+    //Fetching questions from json-server
+    const fetchQuestions = async () =>{
+        const res = await fetch('http://localhost:5001/questions');
+        const data = await res.json();
+        return data;
+    }
 
     //Add a question function
-    const addQuestion = (addQ) =>{
+    const addQuestion = async (addQ) =>{
+        const res = await fetch('http://localhost:5001/questions',{
+            method: 'POST',
+            headers:{
+                'Content-type':'application/json',
+            },
+            body:JSON.stringify(addQ),
+        })
+
+        const data = await res.json();
+        setQuestions([...questions, data]);
+        /*
        const id = Math.floor(Math.random() * 10000) + 1;
        const newQuestion =  {id, ...addQ};
        setQuestions([...questions,newQuestion]);
+       */
     }
 
     //Delete a question function
-    const deleteQuestion = (id) =>{
-        setQuestions(questions.filter((eQuestion) => eQuestion.id !==id))
+    const deleteQuestion = async(id) =>{
+        await fetch(`http://localhost:5001/questions/${id}`,{
+            method:'DELETE',
+        });
+        setQuestions(questions.filter((eQuestion) => eQuestion.id !==id));
     }
     return(
         
