aff9c957e7d587ca9afe1b04238ea9ccc60743ce
LesegoLSG <217057620@student.uj.ac.za>
2023-08-31 13:53:38 -0500

    Designed divs for displaying results

--- src/Components/DashDisplay.js
+++ src/Components/DashDisplay.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import '../Stylesheet/DashDisplay.css';
 
 
 
-const DashDisplay = ({rowCount}) =>{
-    
-    return(
-        <div >
-            <h1>number of questions:{rowCount}</h1>
-           
+const DashDisplay = ({ rowCount }) => {
+
+    return (
+        <div className="main-dash-container">
+            <h1>Dashboard</h1>
+            <div className="sub-dash-grid-containers">
+                <div className="cont numofquestions">
+                    <p>Number of questions:</p>
+                    <h1>145</h1>
+                </div>
+                <div className="cont numofagree">
+                    <p>Agreements:</p>
+                    <h1>145</h1>
+                </div>
+                <div className="cont numofdisagree">
+                    <p>Disagreements:</p>
+                    <h1>145</h1>
+                </div>
+                <div className='cont numofneutral'>
+                    <p>Neutrals:</p>
+                    <h1>145</h1>
+                </div>
+            </div>
+
         </div>
     );
 };
