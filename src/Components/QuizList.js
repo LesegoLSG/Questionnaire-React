9e4ed98f2b09ed4003e284da5b717b88a47aeb3a
LesegoLSG <217057620@student.uj.ac.za>
2023-09-01 11:40:01 -0500

    Implemented the handleAnswerChange and handleSurveySubmit functions

--- src/Components/QuizList.js
+++ src/Components/QuizList.js
@@ -1,31 +1,59 @@
 import React from 'react';
 import Quiz from '../Components/Quiz';
 import { useState } from 'react'
+import DashDisplay from './DashDisplay';
 
 
-const QuizList = ({ questions}) => {
- 
+const QuizList = ({ questions }) => {
+    const [userAnswer, setUserAnswer] = useState({});
+
+    const handleAnswerChange = (questionId, answer) => {
+        setUserAnswer((prevAnswers) => ({
+            ...prevAnswers,
+            [questionId]: answer,
+        }));
+    };
+
+    const handleSurveySubmit = (e) => {
+        e.preventDefault();
+        const results = questions.map((question) => ({
+            questionId: question.id,
+            agree: (userAnswer[question.id] === "agree") ? 1 : 0,
+            neutral: (userAnswer[question.id] === "neutral") ? 1 : 0,
+            disagree: (userAnswer[question.id] === "disagree") ? 1 : 0,
+
+            // Send results to a parent component or display them directly in Dashboard.js
+            // You can pass the 'results' array to a parent component, which then passes it to Dashboard.
+            
+        }));
+        console.log("submit survey button")
+        console.log(results);
+       {/*<DashDisplay  results={results}/> */} 
+    }
+
+   
+
     return (
-        
+
         <form>
             <div className="main-Quiz">
                 {questions.map((quest) => (
                     <Quiz
                         key={quest.id}
                         quest={quest}
+                        //onChange
+                        onAnswerChange = {handleAnswerChange}
                     />
-                    
+
                 ))}
-                <p>Agree: </p>
-                <p>neutral: </p>
-                <p>Disagree: </p>
+               
 
             </div>
-            <button>Submit Quiz answers</button>
+            <button onClick={handleSurveySubmit}>Submit Quiz answers</button>
 
         </form>
     )
-     
+
 };
 
 export default QuizList;
